0db03862e9039fd93cf15e5c4e053fbae335abde
Anhelina <avelichko469@gmail.com>
2026-06-07 15:43:37 +0300

    create spring application

---
+++ src/test/java/org/deeper/crackerarchery/CrackerArcheryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.deeper.crackerarchery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrackerArcheryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
